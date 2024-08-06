bd2fe82ca03725b1bf7aa57ff0ded59dc61b1711
Thobani660 <zondit660@gmail.com>
2024-08-06 16:01:41 +0200

    latest

--- vite-project/src/components/register.jsx
+++ vite-project/src/components/register.jsx
@@ -1,4 +1,5 @@
-function Register({boook}){
+import AddBook from "./createBook"
+function Register({book}){
 
     // ISBN:1343-212-233-554,
     // TITTLE: "JackOfAllTrades1",
@@ -9,26 +10,28 @@ function Register({boook}){
     // FORMET:"A4",
     // COVERDESIGN:"https://cdn.dribbble.com/userupload/9498135/file/original-41f4f441c8ec52d782e7e5db7333a166.jpg?resize=1024x768",
     // RELEASE_DATE:"05/08/2024"
-    console.log("this it is")
+    console.log("this it is",book.TITTLE)
     function Thumela(){
-        alert("Inkinga iku Brawuza,usalungiswa")
+        alert("Inkinga iku Brawuza,usalungiswa");
+        console.log("Inkinga iku Brawuza,usalungiswa")
     }
     return (
         <>
+        <AddBook/>
             <form action="" className="form">
-                    <input style={{width:"350px"}} type="text" name="" id="" placeholder="AuthersName" />
+                    <input style={{width:"350px"}} type="text" name="" id="" placeholder={book.PUBLSH} />
                     <div style={{display:"flex"}}>
                         
-                        <input style={{marginLeft:"60px",width:"220px"}} type="text" name="" id="" placeholder="Tittle" />
-                        <input style={{width:"70px" ,borderRadius:"20px",marginLeft:"50px"}} type="text" name="" id=" " placeholder="ReleaseDate" />
+                        <input style={{marginLeft:"60px",width:"220px"}} type="text" name="" id="" placeholder={book.TITTLE}/>
+                        <input style={{width:"70px" ,borderRadius:"20px",marginLeft:"50px"}} type="text" name="" id=" "placeholder={book.RELEASE_DATE}/>
                     </div>
                     <div>
-                        <input style={{width:"350px"}} type="text" name="" id="" placeholder="AuthersName" />
+                        <input style={{width:"350px"}} type="text" name="" id="" placeholder={book.EDITION} />
 
                     </div>
                     <div>
-                            <input style={{marginLeft:"20px",width:"220px"}} type="text" name="" id="" placeholder="formet" />
-                            <input style={{width:"70px" ,borderRadius:"20px",marginLeft:"50px"}} type="text" name="" id="" placeholder="numb" />
+                            <input style={{marginLeft:"20px",width:"220px"}} type="text" name="" id="" placeholder="Nuumber of pages" />
+                            <input style={{width:"70px" ,borderRadius:"20px",marginLeft:"50px"}} type="text" name="" id="" placeholder={book.NU_PG} />
                     </div>
                      <button style={{height:"30px",width:"70px",borderRadius:"10px",backgroundColor:"lightgreen"}} onClick={Thumela}>submit</button>
             </form>
